c5a1057ca7b3b83757336615e3a3f3c9b2c75d40
dwikaaaa <dwika2800@gmail.com>
2026-09-21 22:52:11 +0700

    feat: users migrations dan fiturs migration untuk memisah peran masing-masing admin(admin pkl, ppdb dll)

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,9 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('username')->unique();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum('role', ['admin', 'user', 'guru', 'kepala_sekolah', 'super_admin', 'super_duper_admin', 'pelanggan']);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
